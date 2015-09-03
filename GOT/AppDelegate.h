661529df937dd1110787e57acc68695374ffe735
Marek <marek.krzysztof.helak@gmail.com>
2015-09-03 20:35:47 +0200

    Initial Commit

---
+++ GOT/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GOT
+//
+//  Created by Marek on 03.09.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GOT/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GOT
+//
+//  Created by Marek on 03.09.2015.
+//  Copyright (c) 2015 Marek Helak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
